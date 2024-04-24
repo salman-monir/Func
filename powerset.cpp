e400f77416e3fd64d815bd04014d001ee2c91c5d
salman-monir <monirraaz@gmail.com>
2024-04-23 19:48:06 -0700

    Update powerset.cpp

--- powerset.cpp
+++ powerset.cpp
@@ -1,1 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+#include<vector>
+//#define pb push_back
+int main()
+{
+    int n;
+    cin>>n;
+    vector<int>v[(1<<n)];
+    int a[n];
+    for(int i=0;i<n;i++)
+        cin>>a[i];
+        //a[i]=i;
+    for(int i=0;i<=(1<<n)-1;i++){
+        for(int bit=0;bit<n;bit++){
+            if(i & (1<<bit)){
+                v[i].push_back(a[bit]);
+            }
+        }
+    }
+        for(int i=0;i<=(1<<n)-1;i++){
+            for(auto j:v[i]){
+                cout<<j<<" ";
+            }
+            cout<<endl;
+        }
 
+}
